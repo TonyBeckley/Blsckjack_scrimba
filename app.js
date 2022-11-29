639caae0ce668bce415529e5f5bc9070788b4c5e
TonyBeckley <tonybeckley43@gmail.com>
2022-11-29 17:19:10 +0100

    app.js not working

---
+++ app.js
@@ -0,0 +1,55 @@
+let hasBlackjack = false
+let isAlive = false
+let message = document.getElementById("messageEl")
+let card = document.getElementById("card-fn")
+let sumFn = documet.getElementById("sum-fn")
+let messageEl = "" 
+let sum = 0
+let firstCard 
+let secondCard
+let cards
+
+function randGen (){
+    Math.floor(Math.random() * 13) + 1
+} 
+
+function startFn (){
+    isAlive = true
+
+    firstCard = randGen()
+    secondCard = randGen()
+
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
+    renderGen()
+}
+
+function renderGen(){
+    card.textContent = "Cards: "
+    for(let i = 0; i < cards.length; i++){
+        card.textContent += cards[i] + " "
+    }
+
+    sumFn.textContent = "Sum: " + sum
+    if(sum <= 20) {
+        message.textContent = "Do you want to draw a new card" 
+    }
+    else if (sum === 21) {
+        message.textContent = "You've got blackjack"
+        hasBlackjack = true
+    }
+    else{
+        message.textContent = "You're out of the game"
+        isAlive = false
+    }
+}
+  
+function newCard(){
+    if (hasBlackjack === false && isAlive === true){
+        let newDraw = randGen()
+        sum += newDraw
+        cards.push(cards)
+        renderGen()
+    }
+}
